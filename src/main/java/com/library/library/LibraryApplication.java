7a5dbe1b11edcd3c16abd23ae69c05c59bf4b9d2
Aounil <yassinaounil5@gmail.com>
2025-06-25 13:29:21 +0100

    issuse

--- src/main/java/com/library/library/LibraryApplication.java
+++ src/main/java/com/library/library/LibraryApplication.java
@@ -1,6 +1,6 @@
 package com.library.library;
 
-import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,12 +9,7 @@ public class LibraryApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.load();
 
-		System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
-		System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
-		System.setProperty("SMTP_EMAIL", dotenv.get("SMTP_EMAIL"));
-		System.setProperty("SMTP_PASSWORD", dotenv.get("SMTP_PASSWORD"));
 		SpringApplication.run(LibraryApplication.class, args);
 	}
 
